11899b1f92362025936ba221f8cf30b3a8e8f176
1XSTRY <kosogornov99@gmail.com>
2023-10-08 23:06:47 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1();
+        //task2();
+        //task3();
+        //task4();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+
+
+
+
+
+}
